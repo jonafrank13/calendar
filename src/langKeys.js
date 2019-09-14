c6a4176b8b8b2d7bde5ec1d0a84695c706236d56
jonafrankbrimma <jona.frank@brimmatech.com>
2019-09-14 18:51:46 +0530

    v4.0 code updates

---
+++ src/langKeys.js
@@ -0,0 +1,50 @@
+export default {
+    'en-us': {
+        app_title_1: 'OPEX ANALYTICS',
+        app_title_2: 'MEETING SCHEDULER',
+        lang: 'Language',
+        new: 'New',
+        edit: 'Edit',
+        meeting: 'Meeting',
+        meet_title: 'Add meeting title',
+        meet_desc: 'Meeting Description',
+        more_opts: 'More Options',
+        delete_meet: 'Delete Meeting',
+        update: 'Update',
+        change: 'Change Language',
+        search: 'Search',
+        save: 'Save'
+    },
+    'ru-ru': {
+        app_title_1: 'OPEX аналитика',
+        app_title_2: 'планировщик встреч',
+        lang: 'язык',
+        new: 'новый',
+        edit: 'Pедактировать',
+        meeting: 'Встреча',
+        meet_title: 'Добавить название встречи',
+        meet_desc: 'Описание встречи',
+        more_opts: 'больше опций',
+        delete_meet: 'удалить встречу',
+        update: 'Обновить',
+        change: 'изменить язык',
+        search: 'поиск',
+        save: 'спасти'
+    },
+    'zh': {
+        app_title_1: 'OPEX 分析',
+        app_title_2: '会议日程安排',
+        lang: '语言',
+        new: '新',
+        edit: '编辑',
+        meeting: '会议',
+        meet_title: '添加会议标题',
+        meet_desc: '会议说明',
+        more_opts: '更多选择',
+        delete_meet: '删除会议',
+        update: '更新',
+        change: '改变语言',
+        search: '搜索',
+        save: '救'
+    }
+}
