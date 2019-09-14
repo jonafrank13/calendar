4021c372b80a811e49c9dae68eb5c87aa375774a
jonafrankbrimma <jona.frank@brimmatech.com>
2019-09-14 12:41:18 +0530

    v2.0 code updates

---
+++ src/assets/sample-data.js
@@ -0,0 +1,247 @@
+export default [
+    {
+        "date": "2019-09-04",
+        "time_from": "16:00",
+        "time_to": "16:30",
+        "title": "Meeting with Brian",
+        "desc": "Meeting with Brian for project report"
+    },
+    {
+        "date": "2019-09-01",
+        "time_from": "10:00",
+        "time_to": "10:30",
+        "title": "Product Team Cadence",
+        "desc": "Daily Standup"
+    },
+    {
+        "date": "2019-09-02",
+        "time_from": "10:00",
+        "time_to": "10:30",
+        "title": "Product Team Cadence",
+        "desc": "Daily Standup"
+    },
+    {
+        "date": "2019-09-03",
+        "time_from": "10:00",
+        "time_to": "10:30",
+        "title": "Product Team Cadence",
+        "desc": "Daily Standup"
+    },
+    {
+        "date": "2019-09-04",
+        "time_from": "10:00",
+        "time_to": "10:30",
+        "title": "Product Team Cadence",
+        "desc": "Daily Standup"
+    },
+    {
+        "date": "2019-09-05",
+        "time_from": "10:00",
+        "time_to": "10:30",
+        "title": "Product Team Cadence",
+        "desc": "Daily Standup"
+    },
+    {
+        "date": "2019-09-06",
+        "time_from": "10:00",
+        "time_to": "10:30",
+        "title": "Product Team Cadence",
+        "desc": "Daily Standup"
+    },
+    {
+        "date": "2019-09-07",
+        "time_from": "10:00",
+        "time_to": "10:30",
+        "title": "Product Team Cadence",
+        "desc": "Daily Standup"
+    },
+    {
+        "date": "2019-09-08",
+        "time_from": "10:00",
+        "time_to": "10:30",
+        "title": "Product Team Cadence",
+        "desc": "Daily Standup"
+    },
+    {
+        "date": "2019-09-09",
+        "time_from": "10:00",
+        "time_to": "10:30",
+        "title": "Product Team Cadence",
+        "desc": "Daily Standup"
+    },
+    {
+        "date": "2019-09-10",
+        "time_from": "10:00",
+        "time_to": "10:30",
+        "title": "Product Team Cadence",
+        "desc": "Daily Standup"
+    },
+    {
+        "date": "2019-09-11",
+        "time_from": "10:00",
+        "time_to": "10:30",
+        "title": "Product Team Cadence",
+        "desc": "Daily Standup"
+    },
+    {
+        "date": "2019-09-12",
+        "time_from": "10:00",
+        "time_to": "10:30",
+        "title": "Product Team Cadence",
+        "desc": "Daily Standup"
+    },
+    {
+        "date": "2019-09-13",
+        "time_from": "10:00",
+        "time_to": "10:30",
+        "title": "Product Team Cadence",
+        "desc": "Daily Standup"
+    },
+    {
+        "date": "2019-09-14",
+        "time_from": "10:00",
+        "time_to": "10:30",
+        "title": "Product Team Cadence",
+        "desc": "Daily Standup"
+    },
+    {
+        "date": "2019-09-15",
+        "time_from": "10:00",
+        "time_to": "10:30",
+        "title": "Product Team Cadence",
+        "desc": "Daily Standup"
+    },
+    {
+        "date": "2019-09-16",
+        "time_from": "10:00",
+        "time_to": "10:30",
+        "title": "Product Team Cadence",
+        "desc": "Daily Standup"
+    },
+    {
+        "date": "2019-09-17",
+        "time_from": "10:00",
+        "time_to": "10:30",
+        "title": "Product Team Cadence",
+        "desc": "Daily Standup"
+    },
+    {
+        "date": "2019-09-18",
+        "time_from": "10:00",
+        "time_to": "10:30",
+        "title": "Product Team Cadence",
+        "desc": "Daily Standup"
+    },
+    {
+        "date": "2019-09-19",
+        "time_from": "10:00",
+        "time_to": "10:30",
+        "title": "Product Team Cadence",
+        "desc": "Daily Standup"
+    },
+    {
+        "date": "2019-09-20",
+        "time_from": "10:00",
+        "time_to": "10:30",
+        "title": "Product Team Cadence",
+        "desc": "Daily Standup"
+    },
+    {
+        "date": "2019-09-21",
+        "time_from": "10:00",
+        "time_to": "10:30",
+        "title": "Product Team Cadence",
+        "desc": "Daily Standup"
+    },
+    {
+        "date": "2019-09-22",
+        "time_from": "10:00",
+        "time_to": "10:30",
+        "title": "Product Team Cadence",
+        "desc": "Daily Standup"
+    },
+    {
+        "date": "2019-09-23",
+        "time_from": "10:00",
+        "time_to": "10:30",
+        "title": "Product Team Cadence",
+        "desc": "Daily Standup"
+    },
+    {
+        "date": "2019-09-24",
+        "time_from": "10:00",
+        "time_to": "10:30",
+        "title": "Product Team Cadence",
+        "desc": "Daily Standup"
+    },
+    {
+        "date": "2019-09-25",
+        "time_from": "10:00",
+        "time_to": "10:30",
+        "title": "Product Team Cadence",
+        "desc": "Daily Standup"
+    },
+    {
+        "date": "2019-09-26",
+        "time_from": "10:00",
+        "time_to": "10:30",
+        "title": "Product Team Cadence",
+        "desc": "Daily Standup"
+    },
+    {
+        "date": "2019-09-27",
+        "time_from": "10:00",
+        "time_to": "10:30",
+        "title": "Product Team Cadence",
+        "desc": "Daily Standup"
+    },
+    {
+        "date": "2019-09-28",
+        "time_from": "10:00",
+        "time_to": "10:30",
+        "title": "Product Team Cadence",
+        "desc": "Daily Standup"
+    },
+    {
+        "date": "2019-09-29",
+        "time_from": "10:00",
+        "time_to": "10:30",
+        "title": "Product Team Cadence",
+        "desc": "Daily Standup"
+    },
+    {
+        "date": "2019-09-30",
+        "time_from": "10:00",
+        "time_to": "10:30",
+        "title": "Product Team Cadence",
+        "desc": "Daily Standup"
+    },
+    {
+        "date": "2019-09-18",
+        "time_from": "12:00",
+        "time_to": "13:30",
+        "title": "Team Lunch",
+        "desc": "Monthly Team Lunch"
+    },
+    {
+        "date": "2019-09-07",
+        "time_from": "14:00",
+        "time_to": "16:00",
+        "title": "1:1 with Mike & Ganesh",
+        "desc": "One on one sessions"
+    },
+    {
+        "date": "2019-09-22",
+        "time_from": "08:00",
+        "time_to": "09:30",
+        "title": "Product Release Meeting",
+        "desc": "release meeting"
+    },
+    {
+        "date": "2019-09-13",
+        "time_from": "08:00",
+        "time_to": "09:30",
+        "title": "Team Huddle",
+        "desc": "general meeting"
+    }
+]
